04de924f3ce3f4dafe6894177f5b358adb16d961
v-trof <vsevolod.trofimow@ya.ru>
2022-07-16 10:47:12 +0300

    Added goblin spawner

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyManager : MonoBehaviour
+{
+    private float _nextSpawnTime;
+    public float spawnCooldown = 1f;
+
+    public GameObject enemy;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        MaybeSpawnGoblin();
+    }
+
+    private void MaybeSpawnGoblin()
+    {
+        if(_nextSpawnTime >= Time.time) return;
+        _nextSpawnTime = Time.time + spawnCooldown;
+        
+        var newGoblin = Instantiate(enemy, transform.position + new Vector3(0, 0.5f, Random.Range(-1.5f, +1.5f)), Quaternion.identity);
+        newGoblin.transform.Rotate(90f, 0, 0);
+    }
+}
