0a54d2eb0841878c5c368ed1f5ab77e67a68658d
Igor Koval <comradekoval@gmail.com>
2022-07-16 21:12:11 +0300

    Fixed line renderer

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -79,10 +79,13 @@ public abstract class Dice : MonoBehaviour
         
         if (_shouldFollowCursor)
         {
-            var pos = transform.position;
-            _lineRenderer.enabled = true;
-            _lineRenderer.SetPositions(new []{pos, pos + direction});
             transform.position = position;
+        }
+
+        if (_forceSelectorActive)
+        {
+            var pos = transform.position;
+            _lineRenderer.SetPositions(new []{pos, pos + direction});
         }
         
         if (_forceSelectorActive && Input.GetButtonUp("Fire1"))
@@ -101,6 +104,7 @@ public abstract class Dice : MonoBehaviour
         {
             _forceSelectorActive = true;
             _shouldFollowCursor = false;
+            _lineRenderer.enabled = true;
             return;
         }
 
